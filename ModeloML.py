ee806a313b2bee7ec06474bf02ac25e1a109b5c1
Rafael Cardoso <84107567+RafaelCardoso140701@users.noreply.github.com>
2026-08-03 14:52:52 -0300

    Update ModeloML.py

--- ModeloML.py
+++ ModeloML.py
@@ -1,11 +1,11 @@
 # Importando o arquivo de teste
 import pandas as pd
-teste = pd.read_csv('/Users/llucaslleall/Desktop/Minicurso/6 Deploy/Usuarios/casas_teste.csv')
+teste = pd.read_csv('C:/Users/PC/Downloads/casas_teste.csv')
 
 # Fazendo o load do modelo
 import sklearn
 from joblib import load
-reg = load('/Users/llucaslleall/Desktop/Minicurso/6 Deploy/Modelo/regressao.joblib')
+reg = load('C:/Users/PC/Downloads/regressao.joblib')
 
 # Fazendo a previsão
 teste = teste.drop('Unnamed: 0',axis=1)
@@ -16,4 +16,4 @@ X_teste = teste.drop('MedHouseVal',axis=1)
 previsao = reg.predict(X_teste)
 X_teste['previsao'] = previsao
 import openpyxl
-X_teste.to_excel('/Users/llucaslleall/Desktop/Minicurso/6 Deploy/Usuarios/previsoes_modelo.xlsx')
+X_teste.to_excel('C:/Users/PC/Downloads/previsoes_modelo.xlsx')
